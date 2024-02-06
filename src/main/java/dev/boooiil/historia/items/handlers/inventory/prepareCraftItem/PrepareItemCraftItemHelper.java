e24a7ac649c3163a44c57784eacdc5e253f42d3b
boooiil <boooiil@outlook.com>
2024-02-06 06:46:43 -0500

    mv, rename

--- src/main/java/dev/boooiil/historia/items/handlers/inventory/prepareCraftItem/PrepareItemCraftItemHelper.java
+++ src/main/java/dev/boooiil/historia/items/handlers/inventory/prepareCraftItem/PrepareItemCraftItemHelper.java
@@ -1,7 +1,6 @@
-package dev.boooiil.historia.items.handlers.inventory;
+package dev.boooiil.historia.items.handlers.inventory.prepareCraftItem;
 
 import dev.boooiil.historia.items.configuration.ConfigurationLoader;
-import dev.boooiil.historia.items.handlers.inventory.prepareCraftItem.PrepareItemCraftInventoryHelper;
 import dev.boooiil.historia.items.items.craftable.CraftedItem;
 import dev.boooiil.historia.items.util.Logging;
 import org.bukkit.inventory.ItemStack;
@@ -9,14 +8,14 @@ import org.bukkit.inventory.ItemStack;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CraftItemManager {
+public class PrepareItemCraftItemHelper {
 
     private ItemStack result;
     private final List<CraftedItem> matchingItems;
     private final ArrayList<String> materials;
     private final ArrayList<String> fullMaterials;
 
-    public CraftItemManager(PrepareItemCraftInventoryHelper inspector) {
+    public PrepareItemCraftItemHelper(PrepareItemCraftInventoryHelper inspector) {
 
         ArrayList<String> patterns = inspector.getPattern();
         materials = inspector.getMaterials();
@@ -39,11 +38,11 @@ public class CraftItemManager {
 
                 if (item.isShapeDependent()) {
 
-                    result = new GetItemMatchingMaterials(item, materials).getItem();
+                    result = getItemMatchingMaterials(item, materials);
 
                 } else {
 
-                    result = new GetItemMatchingMaterials(item, fullMaterials).getItem();
+                    result = getItemMatchingMaterials(item, fullMaterials);
 
                 }
 
@@ -58,6 +57,41 @@ public class CraftItemManager {
 
     public ItemStack getResult() {
         return result;
+    }
+
+    private ItemStack getItemMatchingMaterials(CraftedItem item, List<String> materials) {
+        String replace = "LOW_|MEDIUM_|HIGH_";
+
+        int need = materials.size();
+        int matched = 0;
+
+        if (materials.size() == item.getRecipeItems().size()) {
+
+            for (String material : materials) {
+
+                Logging.debugToConsole(
+                        "[GIMM] Contains Material? " + item.getRecipeItems() + " "
+                                + material.replaceFirst(replace, ""));
+
+                if (item.getRecipeItems().contains(material.replaceFirst(replace, "")))
+                    matched++;
+
+            }
+
+            Logging.debugToConsole("[GIMM] " + item.getItemStack().getType());
+            Logging.debugToConsole("[GIMM] need: " + need);
+            Logging.debugToConsole("[GIMM] matched: " + matched);
+
+            if (matched == need) {
+
+                Logging.debugToConsole("[GIMM] Item: " + item.getItemStack().getType());
+                return item.getItemStack();
+
+            }
+
+        }
+
+        return null;
     }
 
 }
