cc4d6e8a11a004681bb7f041d460cd9fd34cdeaf
boooiil <boooiil@outlook.com>
2024-02-09 19:27:27 -0500

    update javadoc

--- src/main/java/dev/boooiil/historia/items/configuration/items/ItemConfigurationFactory.java
+++ src/main/java/dev/boooiil/historia/items/configuration/items/ItemConfigurationFactory.java
@@ -3,10 +3,28 @@ package dev.boooiil.historia.items.configuration.items;
 import dev.boooiil.historia.items.configuration.BaseConfigurationLoader;
 import dev.boooiil.historia.items.configuration.ConfigurationFactory;
 
+/**
+ * It is a factory class that creates instances of a class extending
+ * {@link BaseConfigurationLoader}.
+ * 
+ * @param <IC> The class type of the {@link BaseConfigurationLoader}.
+ */
 public class ItemConfigurationFactory<IC extends BaseConfiguration> {
 
+    /**
+     * <p>
+     * Base class with extension of type {@code IC}.
+     * </p>
+     * <p>
+     * This variable is used to store instances of a class extending {@link IC}.
+     * </p>
+     */
     private final BaseConfigurationLoader<IC> configurationLoader;
 
+    /**
+     * 
+     * @param clazz The class type of the {@link BaseConfigurationLoader}.
+     */
     public ItemConfigurationFactory(Class<IC> clazz) {
         this.configurationLoader = ConfigurationFactory.getConfigurationLoader(clazz);
     }
