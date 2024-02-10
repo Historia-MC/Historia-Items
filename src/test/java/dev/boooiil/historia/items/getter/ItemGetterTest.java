48894f03d5a062f480e055ddd90da6fa634464d6
boooiil <boooiil@outlook.com>
2024-02-09 22:27:56 -0500

    rm ConfigurationProvider for ItemConfigurationFactory

--- src/test/java/dev/boooiil/historia/items/getter/ItemGetterTest.java
+++ src/test/java/dev/boooiil/historia/items/getter/ItemGetterTest.java
@@ -13,7 +13,6 @@ import be.seeseemelk.mockbukkit.entity.PlayerMock;
 import dev.boooiil.historia.items.file.FileIO;
 import dev.boooiil.historia.items.file.FileKeys;
 import dev.boooiil.historia.items.Main;
-import dev.boooiil.historia.items.configuration.ConfigurationProvider;
 import dev.boooiil.historia.items.configuration.items.ArmorConfiguration;
 import dev.boooiil.historia.items.configuration.items.CustomItemConfiguration;
 import dev.boooiil.historia.items.configuration.items.ItemConfigurationFactory;
