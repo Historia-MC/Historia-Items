0244ad69b5d733bf1736ae86b583350bfd2d7bda
boooiil <boooiil@outlook.com>
2024-02-15 14:28:33 -0500

    return BaseItem from parsed inventory

--- src/main/java/dev/boooiil/historia/items/handlers/inventory/prepareCraftItem/PrepareItemCraftHandler.java
+++ src/main/java/dev/boooiil/historia/items/handlers/inventory/prepareCraftItem/PrepareItemCraftHandler.java
@@ -1,8 +1,17 @@
 package dev.boooiil.historia.items.handlers.inventory.prepareCraftItem;
 
+import org.bukkit.Material;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.inventory.ItemStack;
+
+import dev.boooiil.historia.items.configuration.items.ArmorConfiguration;
+import dev.boooiil.historia.items.configuration.items.CustomItemConfiguration;
+import dev.boooiil.historia.items.configuration.items.ToolConfiguration;
+import dev.boooiil.historia.items.configuration.items.WeaponConfiguration;
+import dev.boooiil.historia.items.crafted.BaseItem;
+import dev.boooiil.historia.items.crafted.CraftedItemFactory;
+import dev.boooiil.historia.items.crafted.weapon.Weapon;
 
 public class PrepareItemCraftHandler {
 
@@ -17,23 +26,44 @@ public class PrepareItemCraftHandler {
 
     public void parseInventoryAndGiveResult() {
 
-        ItemStack item = parseInventory();
+        BaseItem item = parseInventory();
 
         if (item == null)
             event.getInventory().setResult(event.getRecipe().getResult());
         else
-            event.getInventory().setResult(item);
+            event.getInventory().setResult(item.getItemStack());
 
     }
 
-    private ItemStack parseInventory() {
+    private BaseItem parseInventory() {
 
         PrepareItemCraftInventoryHelper helper = new PrepareItemCraftInventoryHelper(craftingInventory.getMatrix());
         PrepareItemCraftItemHelper itemHelper = new PrepareItemCraftItemHelper(helper);
 
         itemHelper.doMatch();
 
-        return itemHelper.getResult();
+        if (itemHelper.getResult() == null)
+            return new BaseItem(new ItemStack(Material.AIR), false);
+
+        if (itemHelper.getResult() instanceof ArmorConfiguration) {
+            return CraftedItemFactory.createArmor((ArmorConfiguration) itemHelper.getResult());
+        }
+
+        else if (itemHelper.getResult() instanceof WeaponConfiguration) {
+            return new Weapon((WeaponConfiguration) itemHelper.getResult());
+        }
+
+        else if (itemHelper.getResult() instanceof ToolConfiguration) {
+            return CraftedItemFactory.createTool((ToolConfiguration) itemHelper.getResult());
+        }
+
+        else if (itemHelper.getResult() instanceof CustomItemConfiguration) {
+            return CraftedItemFactory.createCustom((CustomItemConfiguration) itemHelper.getResult());
+        }
+
+        else {
+            return new BaseItem(event.getRecipe().getResult(), false);
+        }
 
     }
 
