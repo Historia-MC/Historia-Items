ae0e32c64beffefb920e577a7f8d2b0e5e03d4b7
boooiil <boooiil@outlook.com>
2024-02-19 13:33:53 -0500

    use registry to obtain item

--- src/main/java/dev/boooiil/historia/items/commands/CommandGive.java
+++ src/main/java/dev/boooiil/historia/items/commands/CommandGive.java
@@ -1,16 +1,17 @@
 package dev.boooiil.historia.items.commands;
 
-import dev.boooiil.historia.items.configuration.ConfigurationProvider;
-import dev.boooiil.historia.items.configuration.items.ArmorConfiguration;
-import dev.boooiil.historia.items.configuration.items.ArmorConfigurationLoader;
-import dev.boooiil.historia.items.configuration.items.CustomItemConfigurationLoader;
-import dev.boooiil.historia.items.configuration.items.CustomItemConfiguration;
-import dev.boooiil.historia.items.configuration.items.WeaponConfigurationLoader;
+import dev.boooiil.historia.items.configuration.ItemConfigurationRegistry;
+import dev.boooiil.historia.items.configuration.crafted.BaseItemConfiguration;
+import dev.boooiil.historia.items.configuration.crafted.armor.ArmorConfiguration;
+import dev.boooiil.historia.items.configuration.crafted.custom.CustomConfiguration;
+import dev.boooiil.historia.items.configuration.crafted.tool.ToolConfiguration;
+import dev.boooiil.historia.items.configuration.crafted.weapon.WeaponConfiguration;
 import dev.boooiil.historia.items.crafted.armor.Armor;
 import dev.boooiil.historia.items.crafted.custom.Custom;
+import dev.boooiil.historia.items.crafted.tool.Tool;
 import dev.boooiil.historia.items.crafted.weapon.Weapon;
-import dev.boooiil.historia.items.configuration.items.WeaponConfiguration;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -22,68 +23,48 @@ public class CommandGive implements CommandExecutor {
     // It's a method that is called when a command is executed.
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        if (!(sender instanceof Player player))
+        if (args.length > 2) {
+            sender.sendMessage("Syntax: /give <player> <item name>");
             return false;
-        if (args.length == 0)
+        }
+
+        if (Bukkit.getPlayer(args[0]) == null) {
+            sender.sendMessage("That player could not be found.");
             return false;
+        }
 
-        if (args[0].equalsIgnoreCase("weapon")) {
+        Player player = Bukkit.getPlayer(args[0]);
+        BaseItemConfiguration baseConfiguration = ItemConfigurationRegistry.get(args[1]);
 
-            WeaponConfigurationLoader weaponConfig = ConfigurationProvider.getWeaponConfigurationLoader();
+        if (baseConfiguration == null) {
+            sender.sendMessage("Invalid item name.");
+            return false;
+        }
 
-            if (weaponConfig.isValid(args[1])) {
-
-                WeaponConfiguration weaponConfiguration = weaponConfig.getObject(args[1]);
+        switch (baseConfiguration.getItemType()) {
+            case WEAPON:
+                WeaponConfiguration weaponConfiguration = (WeaponConfiguration) baseConfiguration;
                 Weapon weapon = new Weapon(weaponConfiguration);
-
-                player.getWorld().dropItemNaturally(player.getLocation(), weapon.getItemStack());
-
-            }
-
-            else {
-
-                sender.sendMessage("Invalid item name.");
-            }
-
-        } else if (args[0].equalsIgnoreCase("armor")) {
-
-            ArmorConfigurationLoader armorConfig = ConfigurationProvider.getArmorConfigurationLoader();
-
-            if (armorConfig.isValid(args[1])) {
-
-                ArmorConfiguration armorConfiguration = armorConfig.getObject(args[1]);
+                player.getInventory().addItem(weapon.getItemStack());
+                break;
+            case ARMOR:
+                ArmorConfiguration armorConfiguration = (ArmorConfiguration) baseConfiguration;
                 Armor armor = new Armor(armorConfiguration);
-
-                player.getWorld().dropItemNaturally(player.getLocation(), armor.getItemStack());
-
-            }
-
-            else {
-
+                player.getInventory().addItem(armor.getItemStack());
+                break;
+            case CUSTOM:
+                CustomConfiguration customConfiguration = (CustomConfiguration) baseConfiguration;
+                Custom custom = new Custom(customConfiguration);
+                player.getInventory().addItem(custom.getItemStack());
+                break;
+            case TOOL:
+                ToolConfiguration toolConfiguration = (ToolConfiguration) baseConfiguration;
+                Tool tool = new Tool(toolConfiguration);
+                player.getInventory().addItem(tool.getItemStack());
+                break;
+            default:
                 sender.sendMessage("Invalid item name.");
-            }
-
-        } else if (args[0].equalsIgnoreCase("other")) {
-
-            CustomItemConfigurationLoader customItemConfig = ConfigurationProvider.getCustomItemConfigurationLoader();
-
-            if (customItemConfig.isValid(args[1])) {
-
-                CustomItemConfiguration itemConfiguration = customItemConfig.getObject(args[1]);
-                Custom item = new Custom(itemConfiguration);
-
-                player.getWorld().dropItemNaturally(player.getLocation(), item.getItemStack());
-
-            }
-
-            else {
-
-                sender.sendMessage("Invalid item name.");
-            }
-
-        } else {
-            sender.sendMessage("Syntax: /give <weapon/armor/other> <item name>");
-            return false;
+                return false;
         }
 
         return false;
