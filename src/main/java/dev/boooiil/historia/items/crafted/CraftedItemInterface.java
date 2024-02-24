12dd2bd77c4551f1f6c63c75028a0c2028f9b527
boooiil <boooiil@outlook.com>
2024-02-24 13:08:44 -0500

    init, needs work

--- src/main/java/dev/boooiil/historia/items/crafted/CraftedItemInterface.java
+++ src/main/java/dev/boooiil/historia/items/crafted/CraftedItemInterface.java
@@ -4,5 +4,16 @@ import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 interface CraftedItemInterface<T> {
+
     T create(@NotNull ItemStack itemStack);
+
+    ItemStack getItemStack();
+
+    String getDisplayName();
+
+    boolean isValid();
+
+    ItemType getItemType();
+
+    String getID();
 }
