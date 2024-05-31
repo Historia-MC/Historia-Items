a563d69f1f8c9b1024e571c9d37cfe636cd6cba1
boooiil <boooiil@outlook.com>
2024-05-31 05:06:40 -0400

    create enum for material match type

---
+++ src/main/java/dev/boooiil/historia/items/configuration/crafted/MaterialMatchBy.java
@@ -0,0 +1,10 @@
+package dev.boooiil.historia.items.configuration.crafted;
+
+public enum MaterialMatchBy {
+    ALL,
+    PERSISTANT_DATA_STORAGE,
+    LORE,
+    MODIFIER,
+    NAME,
+    LOCALIZED_NAME
+}
