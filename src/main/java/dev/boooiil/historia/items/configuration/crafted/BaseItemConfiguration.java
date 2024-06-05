4ea587e8f57778b8f23f425c0f9964afd45839b9
boooiil <boooiil@outlook.com>
2024-06-05 04:20:54 -0400

    remove redundant variables and getters

--- src/main/java/dev/boooiil/historia/items/configuration/crafted/BaseItemConfiguration.java
+++ src/main/java/dev/boooiil/historia/items/configuration/crafted/BaseItemConfiguration.java
@@ -1,15 +1,11 @@
 package dev.boooiil.historia.items.configuration.crafted;
 
-import dev.boooiil.historia.core.proficiency.Proficiency.ProficiencyName;
 import dev.boooiil.historia.items.crafted.ItemType;
-import dev.boooiil.historia.items.util.Logging;
 
 import java.util.List;
 
 import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
-
-import com.sk89q.worldedit.blocks.BaseItem;
 
 /**
  * <p>
@@ -26,23 +22,10 @@ public abstract class BaseItemConfiguration {
 
     // TODO: ALL items need to have an ID in the configuration file.
     // TODO: ADD an id field to each section.
+    // TODO: set all members to private and add getters and setters.
+
     /** custom id of the item */
     protected String id;
-
-    /**
-     * Recipe shape generally in the pattern of
-     * [ "###", "###", "###" ].
-     */
-    protected List<String> recipeShape;
-
-    /** Materials that are present in the creation of this item. */
-    protected List<String> recipeItems;
-
-    /** The proficiency that can craft this item. */
-    protected List<String> proficiencies;
-
-    /** If the item is shape dependent. */
-    protected boolean isShaped;
 
     /** The material of the item. */
     protected Material material;
@@ -61,71 +44,6 @@ public abstract class BaseItemConfiguration {
 
     /** base item configuration default constructor */
     public BaseItemConfiguration() {
-    }
-
-    /**
-     * This function returns the list of items that are required to craft the item
-     *
-     * @return The list of recipe items.
-     */
-    public List<String> getRecipeItems() {
-
-        return this.recipeItems;
-
-    }
-
-    /**
-     * It returns the recipe shape
-     *
-     * @return The recipe shape.
-     */
-    public List<String> getRecipeShape() {
-
-        return this.recipeShape;
-
-    }
-
-    /**
-     * Validate recipe of items to see if it matches an armor.
-     * 
-     * @param inputItems List of recipe items.
-     * @param inputShape Recipe shape.
-     *
-     * @return If the recipe is valid.
-     */
-    public Boolean isValidRecipe(List<String> inputItems, List<String> inputShape) {
-
-        boolean validItems = this.recipeItems.equals(inputItems);
-        boolean validShape = this.recipeShape.equals(inputShape);
-
-        return validItems && validShape;
-
-    }
-
-    /**
-     * It returns if the item is shape dependent.
-     *
-     * @return If the item is shape dependent.
-     */
-    public boolean isShapeDependent() {
-        return isShaped;
-    }
-
-    /**
-     * It returns whether the given proficiency can craft this item.
-     * 
-     * @param proficiency The proficiency to check.
-     * @return If the proficiency can craft the item.
-     */
-    public boolean canCraft(ProficiencyName proficiency) {
-
-        Logging.debugToConsole("[CraftedItem] Proficiencies: " + this.proficiencies.toString());
-
-        if (this.proficiencies.contains("ALL"))
-            return true;
-        else
-            return this.proficiencies.contains(proficiency.getKey());
-
     }
 
     /**
