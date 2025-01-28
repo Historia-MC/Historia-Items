70085d0da24a61d865f8c97e656df8452ce1a009
boooiil <boooiil@outlook.com>
2025-01-28 16:44:22 -0500

    create interface for item components

---
+++ src/main/java/dev/boooiil/historia/items/configuration/item/components/IItemComponent.java
@@ -0,0 +1,15 @@
+package dev.boooiil.historia.items.configuration.item.components;
+
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.inventory.ItemStack;
+
+public interface IItemComponent {
+
+    public void processConfiguration(ConfigurationSection section);
+
+    public void processConfiguration(YamlConfiguration configuration);
+
+    public ItemStack applyToItemStack(ItemStack item, float qualityModifier);
+
+}
