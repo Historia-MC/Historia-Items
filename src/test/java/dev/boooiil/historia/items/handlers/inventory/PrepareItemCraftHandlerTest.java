c535c865cfd197f852ebed3a4e3338b4ee8baa6a
boooiil <boooiil@outlook.com>
2025-02-04 19:09:55 -0500

    update tests

--- src/test/java/dev/boooiil/historia/items/handlers/inventory/PrepareItemCraftHandlerTest.java
+++ src/test/java/dev/boooiil/historia/items/handlers/inventory/PrepareItemCraftHandlerTest.java
@@ -10,14 +10,19 @@ import org.junit.jupiter.api.Test;
 
 import be.seeseemelk.mockbukkit.MockBukkit;
 import be.seeseemelk.mockbukkit.ServerMock;
+import be.seeseemelk.mockbukkit.entity.EntityMock;
 import be.seeseemelk.mockbukkit.entity.PlayerMock;
+import be.seeseemelk.mockbukkit.entity.ZombieMock;
 import dev.boooiil.historia.items.Main;
+import dev.boooiil.historia.items.configuration.ItemConfigurationRegistry;
+import dev.boooiil.historia.items.util.Logging;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 
 public class PrepareItemCraftHandlerTest {
 
     private ServerMock server;
     private Main plugin;
-    private PlayerMock player;
 
     @BeforeEach
     public void setUp() {
@@ -30,10 +35,6 @@ public class PrepareItemCraftHandlerTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println("Creating player...");
-        player = new PlayerMock(server, "MockUser", UUID.fromString("00000000-0000-0000-0000-000000000001"));
-        System.out.println("Player created: " + player.getName());
-
         System.out.println("Finished setup.");
 
     }
@@ -47,25 +48,36 @@ public class PrepareItemCraftHandlerTest {
     @Test
     public void doMainTest() {
         PlayerMock player = server.addPlayer();
+        Logging.debugToConsole("Creating player", player.getName());
 
-        ItemStack[] items = {
-                new ItemStack(Material.AIR),
-                new ItemStack(Material.IRON_INGOT),
-                new ItemStack(Material.AIR),
+        EntityMock entity = new ZombieMock(server, UUID.randomUUID());
+        Logging.debugToConsole("Creating entity", entity.getType().name());
 
-                new ItemStack(Material.AIR),
-                new ItemStack(Material.IRON_INGOT),
-                new ItemStack(Material.AIR),
+        // player.attack(entity);
 
-                new ItemStack(Material.AIR),
-                new ItemStack(Material.STICK),
-                new ItemStack(Material.AIR)
-        };
+        player.getInventory().addItem(ItemConfigurationRegistry.get("Light_Tin_Sword").createItemStack());
 
-        System.out.println("Crafting item...");
-        ItemStack item = server.craftItem(items, player.getWorld(), player);
+        for (ItemStack item : player.getInventory().getContents()) {
+            if (item != null && item.getType() != Material.AIR) {
+                System.out.println("Item found: " + item.getType());
+            }
+        }
 
-        System.out.println("Item: " + item.getType().toString());
+        System.out.println(player.getInventory().getItemInMainHand().getItemMeta().getDisplayName());
+
+        // if (!player.getInventory().getItemInMainHand().hasItemMeta()
+        // || player.getInventory().getItemInMainHand().getItemMeta().lore() == null
+        // || player.getInventory().getItemInMainHand().getItemMeta().lore().isEmpty())
+        // {
+
+        // return;
+        // }
+
+        for (Component component : player.getInventory().getItemInMainHand().getItemMeta().lore()) {
+            TextComponent textComponent = (TextComponent) component;
+
+            System.out.println(textComponent.content());
+        }
         // do main file tests
     }
 
