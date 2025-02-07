fd2790919f2dc4a36dd2172dcaef42f68c5f7bde
boooiil <boooiil@outlook.com>
2025-02-07 00:07:19 -0500

    apply command placeholder and execute command

--- src/main/java/dev/boooiil/historia/items/item/executor/ItemExecutable.java
+++ src/main/java/dev/boooiil/historia/items/item/executor/ItemExecutable.java
@@ -1,14 +1,21 @@
 package dev.boooiil.historia.items.item.executor;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 
 public class ItemExecutable {
     private List<String> commands;
     // ticks
     private int cooldown;
-    private int ueses;
+    private int uses;
     private boolean hasCooldown;
 
     public ItemExecutable(
@@ -18,13 +25,37 @@ public class ItemExecutable {
             boolean hasCooldown) {
         this.commands = commands;
         this.cooldown = cooldown;
-        this.ueses = uses;
+        this.uses = uses;
         this.hasCooldown = hasCooldown;
 
     }
 
-    public ItemStack execute(ItemStack item) {
+    public ItemStack execute(Player player, ItemStack item) {
+
+        for (String command : applyCommandPlaceholder(player)) {
+
+            player.performCommand(command);
+
+        }
+
+        this.uses--;
+
         return item;
+    }
+
+    private List<String> applyCommandPlaceholder(Player player) {
+        List<String> nCommands = new ArrayList<>();
+
+        for (String command : commands) {
+            String nCommand = ((TextComponent) MiniMessage.miniMessage().deserialize(command,
+                    Placeholder.component("player",
+                            Component.text(player.getName()))))
+                    .content();
+
+            nCommands.add(nCommand);
+        }
+
+        return nCommands;
     }
 
     public List<String> commands() {
@@ -36,7 +67,7 @@ public class ItemExecutable {
     }
 
     public int uses() {
-        return this.ueses;
+        return this.uses;
     }
 
     public boolean hasCooldown() {
