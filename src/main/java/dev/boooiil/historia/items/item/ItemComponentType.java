d794fe2657b7eeee8cac82c64d9770a868c37fb6
Pieter <zotje3000@gmail.com>
2025-02-23 21:58:40 +0100

    add defaultData

--- src/main/java/dev/boooiil/historia/items/item/ItemComponentType.java
+++ src/main/java/dev/boooiil/historia/items/item/ItemComponentType.java
@@ -12,22 +12,43 @@ import java.util.function.Supplier;
 public class ItemComponentType<T extends ItemComponent> {
 
     private final Function<ConfigurationSection, T> fromConfig;
+    private final Supplier<? extends ItemData> defaultData;
 
     public ItemComponentType(
-            Function<ConfigurationSection, T> fromConfig
+            Function<ConfigurationSection, T> fromConfig,
+            Supplier<? extends ItemData> defaultData
     ) {
         this.fromConfig = fromConfig;
+        this.defaultData = defaultData;
     }
 
     public T fromConfig(ConfigurationSection section) {
         return fromConfig.apply(section);
     }
 
+    public ItemData getData() {
+        return defaultData.get();
+    }
+
     static {
-        ItemComponentRegistry.register("tool", new ItemComponentType<>(ToolComponent::fromConfig));
-        ItemComponentRegistry.register("weapon", new ItemComponentType<>(WeaponComponent::fromConfig));
-        ItemComponentRegistry.register("armor", new ItemComponentType<>(ArmorComponent::fromConfig));
-        ItemComponentRegistry.register("executor", new ItemComponentType<>(ExecutorComponent::fromConfig));
-        ItemComponentRegistry.register("runnable", new ItemComponentType<>(RunnableComponent::fromConfig));
+        ItemComponentRegistry.register("tool", new ItemComponentType<>(
+                ToolComponent::fromConfig,
+                () -> new ToolData(1, 1, 1, 1)));
+
+        ItemComponentRegistry.register("weapon", new ItemComponentType<>(
+                WeaponComponent::fromConfig,
+                () -> new WeaponData(1)));
+
+        ItemComponentRegistry.register("armor", new ItemComponentType<>(
+                ArmorComponent::fromConfig,
+                () -> new ArmorData(1, 1)));
+
+        ItemComponentRegistry.register("executor", new ItemComponentType<>(
+                ExecutorComponent::fromConfig,
+                () -> new ExecutorData(new HashMap<>())));
+
+        ItemComponentRegistry.register("runnable", new ItemComponentType<>(
+                RunnableComponent::fromConfig,
+                () -> new RunnableData(0, "", "")));
     }
 }
