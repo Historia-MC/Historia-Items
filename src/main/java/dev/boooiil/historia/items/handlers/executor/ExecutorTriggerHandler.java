3f309275883f5cc5caddaa2c0add279563375ad4
boooiil <boooiil@outlook.com>
2025-03-06 14:27:55 -0500

    complete tests

--- src/main/java/dev/boooiil/historia/items/handlers/executor/ExecutorTriggerHandler.java
+++ src/main/java/dev/boooiil/historia/items/handlers/executor/ExecutorTriggerHandler.java
@@ -1,12 +1,15 @@
 package dev.boooiil.historia.items.handlers.executor;
 
-import org.bukkit.Bukkit;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent; // DONE
 import org.bukkit.event.entity.EntityDropItemEvent; // DONE
 import org.bukkit.event.entity.EntityInteractEvent; // DONE
@@ -23,7 +26,6 @@ import org.bukkit.event.player.PlayerToggleSprintEvent; // DONE
 import org.bukkit.inventory.ItemStack;
 
 import com.destroystokyo.paper.event.player.PlayerJumpEvent; // DONE
-import com.sk89q.worldguard.bukkit.event.block.PlaceBlockEvent;
 
 import dev.boooiil.historia.items.HistoriaItems;
 import dev.boooiil.historia.items.item.HistoriaItemData;
@@ -39,15 +41,10 @@ public class ExecutorTriggerHandler {
 
         Player player = event.getPlayer();
 
-        ItemStack mainItemStack = player.getInventory().getItemInMainHand();
-        ItemStack offItemStack = player.getInventory().getItemInOffHand();
-
         if (event.isSneaking()) {
-            execute(player, mainItemStack, Triggers.CROUCH);
-            execute(player, offItemStack, Triggers.CROUCH);
+            execute(player, Triggers.CROUCH);
         } else {
-            execute(player, mainItemStack, Triggers.UNCROUCH);
-            execute(player, offItemStack, Triggers.UNCROUCH);
+            execute(player, Triggers.UNCROUCH);
         }
     }
 
@@ -58,40 +55,32 @@ public class ExecutorTriggerHandler {
 
         HumanEntity humanEntity = (HumanEntity) event.getEntity().getShooter();
 
-        ItemStack mainItemStack = humanEntity.getInventory().getItemInMainHand();
-        ItemStack offItemStack = humanEntity.getInventory().getItemInOffHand();
-
-        execute(humanEntity, mainItemStack, Triggers.THROW);
-        execute(humanEntity, offItemStack, Triggers.THROW);
+        execute(humanEntity, Triggers.THROW);
     }
 
     public static void executeAction(EntityToggleSwimEvent event) {
+
+        HILogger.debugToConsole("" + (event.getEntity() instanceof HumanEntity),
+                " " + ((HumanEntity) event.getEntity()).isSwimming());
+
         if (!(event.getEntity() instanceof HumanEntity)) {
             return;
         }
 
         HumanEntity humanEntity = (HumanEntity) event.getEntity();
 
-        if (!humanEntity.isSwimming()) {
+        if (!event.isSwimming()) {
             return;
         }
 
-        ItemStack mainItemStack = humanEntity.getInventory().getItemInMainHand();
-        ItemStack offItemStack = humanEntity.getInventory().getItemInOffHand();
-
-        execute(humanEntity, mainItemStack, Triggers.SWIM);
-        execute(humanEntity, offItemStack, Triggers.SWIM);
+        execute(humanEntity, Triggers.SWIM);
     }
 
     public static void executeAction(PlayerToggleSprintEvent event) {
-        if (event.getPlayer().isSprinting()) {
+        if (event.isSprinting()) {
             Player player = event.getPlayer();
 
-            ItemStack mainItemStack = player.getInventory().getItemInMainHand();
-            ItemStack offItemStack = player.getInventory().getItemInOffHand();
-
-            execute(player, mainItemStack, Triggers.SPRINT);
-            execute(player, offItemStack, Triggers.SPRINT);
+            execute(player, Triggers.SPRINT);
         }
     }
 
@@ -99,30 +88,21 @@ public class ExecutorTriggerHandler {
 
         Player player = event.getPlayer();
 
-        ItemStack mainItemStack = player.getInventory().getItemInMainHand();
-        ItemStack offItemStack = player.getInventory().getItemInOffHand();
-
-        execute(player, mainItemStack, Triggers.JUMP);
-        execute(player, offItemStack, Triggers.JUMP);
+        execute(player, Triggers.JUMP);
     }
 
     public static void executeAction(PlayerItemConsumeEvent event) {
 
         Player player = event.getPlayer();
 
-        ItemStack mainItemStack = player.getInventory().getItemInMainHand();
-        ItemStack offItemStack = player.getInventory().getItemInOffHand();
-
         // if bottle
         if (event.getItem().getType() == Material.GLASS_BOTTLE) {
-            execute(player, mainItemStack, Triggers.DRINK);
-            execute(player, offItemStack, Triggers.DRINK);
+            execute(player, Triggers.DRINK);
         }
 
         // else some other consumable
         else {
-            execute(player, mainItemStack, Triggers.EAT);
-            execute(player, offItemStack, Triggers.EAT);
+            execute(player, Triggers.EAT);
         }
 
     }
@@ -134,11 +114,7 @@ public class ExecutorTriggerHandler {
 
         HumanEntity humanEntity = (HumanEntity) event.getEntity();
 
-        ItemStack mainItemStack = humanEntity.getInventory().getItemInMainHand();
-        ItemStack offItemStack = humanEntity.getInventory().getItemInOffHand();
-
-        execute(humanEntity, mainItemStack, Triggers.DROP);
-        execute(humanEntity, offItemStack, Triggers.DROP);
+        execute(humanEntity, Triggers.DROP);
     }
 
     public static void executeAction(EntityPickupItemEvent event) {
@@ -148,32 +124,19 @@ public class ExecutorTriggerHandler {
 
         HumanEntity humanEntity = (HumanEntity) event.getEntity();
 
-        ItemStack mainItemStack = humanEntity.getInventory().getItemInMainHand();
-        ItemStack offItemStack = humanEntity.getInventory().getItemInOffHand();
-
-        execute(humanEntity, mainItemStack, Triggers.PICKUP);
-        execute(humanEntity, offItemStack, Triggers.PICKUP);
+        execute(humanEntity, Triggers.PICKUP);
 
     }
 
-    public static void executeAction(PlaceBlockEvent event) {
+    public static void executeAction(BlockPlaceEvent event) {
 
-        if (!Bukkit.getPluginManager().isPluginEnabled("WorldGuard")) {
-            HILogger.errorToConsole("WorldGuard is not enabled. Cannot execute PlaceBlockEvent.");
-            return;
-        }
-
-        Player player = event.getCause().getFirstPlayer();
+        Player player = event.getPlayer();
 
         if (player == null) {
             return;
         }
 
-        ItemStack mainItemStack = player.getInventory().getItemInMainHand();
-        ItemStack offItemStack = player.getInventory().getItemInOffHand();
-
-        execute(player, mainItemStack, Triggers.PLACE);
-        execute(player, offItemStack, Triggers.PLACE);
+        execute(player, Triggers.PLACE);
 
     }
 
@@ -181,33 +144,21 @@ public class ExecutorTriggerHandler {
 
         Player player = event.getPlayer();
 
-        ItemStack mainItemStack = player.getInventory().getItemInMainHand();
-        ItemStack offItemStack = player.getInventory().getItemInOffHand();
-
-        execute(player, mainItemStack, Triggers.SWAP_TO_MAINHAND);
-        execute(player, offItemStack, Triggers.SWAP_TO_OFFHAND);
+        execute(player, Triggers.SWAP_TO_OFFHAND);
     }
 
     public static void executeAction(InventoryOpenEvent event) {
 
         HumanEntity humanEntity = (HumanEntity) event.getPlayer();
 
-        ItemStack mainItemStack = humanEntity.getInventory().getItemInMainHand();
-        ItemStack offItemStack = humanEntity.getInventory().getItemInOffHand();
-
-        execute(humanEntity, mainItemStack, Triggers.OPEN_INVENTORY);
-        execute(humanEntity, offItemStack, Triggers.OPEN_INVENTORY);
+        execute(humanEntity, Triggers.OPEN_INVENTORY);
     }
 
     public static void executeAction(InventoryCloseEvent event) {
 
         HumanEntity humanEntity = (HumanEntity) event.getPlayer();
 
-        ItemStack mainItemStack = humanEntity.getInventory().getItemInMainHand();
-        ItemStack offItemStack = humanEntity.getInventory().getItemInOffHand();
-
-        execute(humanEntity, mainItemStack, Triggers.CLOSE_INVENTORY);
-        execute(humanEntity, offItemStack, Triggers.CLOSE_INVENTORY);
+        execute(humanEntity, Triggers.CLOSE_INVENTORY);
     }
 
     public static void executeAction(EntityDamageByEntityEvent event) {
@@ -223,11 +174,7 @@ public class ExecutorTriggerHandler {
 
         HumanEntity damager = (HumanEntity) event.getDamager();
 
-        ItemStack mainItemStack = damager.getInventory().getItemInMainHand();
-        ItemStack offItemStack = damager.getInventory().getItemInOffHand();
-
-        execute(damager, mainItemStack, Triggers.DAMAGE_ENTITY);
-        execute(damager, offItemStack, Triggers.DAMAGE_ENTITY);
+        execute(damager, Triggers.DAMAGE_ENTITY);
 
     }
 
@@ -241,37 +188,26 @@ public class ExecutorTriggerHandler {
 
         HumanEntity humanEntity = (HumanEntity) event.getEntity();
 
-        ItemStack mainItemStack = humanEntity.getInventory().getItemInMainHand();
-        ItemStack offItemStack = humanEntity.getInventory().getItemInOffHand();
-
-        execute(humanEntity, mainItemStack, Triggers.INTERACT_ENTITY);
-        execute(humanEntity, offItemStack, Triggers.INTERACT_ENTITY);
+        execute(humanEntity, Triggers.INTERACT_ENTITY);
 
     }
 
     public static void executeAction(PlayerInteractEvent event) {
         Player player = event.getPlayer();
 
-        ItemStack mainItemStack = player.getInventory().getItemInMainHand();
-        ItemStack offItemStack = player.getInventory().getItemInOffHand();
-
         // if block is not air
         if (event.getClickedBlock() != null) {
             if (player.isSneaking()) {
                 if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-                    execute(player, mainItemStack, Triggers.SNEAK_RIGHT_CLICK_BLOCK);
-                    execute(player, offItemStack, Triggers.SNEAK_RIGHT_CLICK_BLOCK);
+                    execute(player, Triggers.SNEAK_RIGHT_CLICK_BLOCK);
                 } else if (event.getAction() == Action.LEFT_CLICK_BLOCK) {
-                    execute(player, mainItemStack, Triggers.SNEAK_LEFT_CLICK_BLOCK);
-                    execute(player, offItemStack, Triggers.SNEAK_LEFT_CLICK_BLOCK);
+                    execute(player, Triggers.SNEAK_LEFT_CLICK_BLOCK);
                 }
             } else {
                 if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-                    execute(player, mainItemStack, Triggers.RIGHT_CLICK_BLOCK);
-                    execute(player, offItemStack, Triggers.RIGHT_CLICK_BLOCK);
+                    execute(player, Triggers.RIGHT_CLICK_BLOCK);
                 } else if (event.getAction() == Action.LEFT_CLICK_BLOCK) {
-                    execute(player, mainItemStack, Triggers.LEFT_CLICK_BLOCK);
-                    execute(player, offItemStack, Triggers.LEFT_CLICK_BLOCK);
+                    execute(player, Triggers.LEFT_CLICK_BLOCK);
                 }
             }
 
@@ -281,46 +217,56 @@ public class ExecutorTriggerHandler {
         else {
             if (player.isSneaking()) {
                 if (event.getAction() == Action.RIGHT_CLICK_AIR) {
-                    execute(player, mainItemStack, Triggers.SNEAK_RIGHT_CLICK);
-                    execute(player, offItemStack, Triggers.SNEAK_RIGHT_CLICK);
+                    execute(player, Triggers.SNEAK_RIGHT_CLICK);
                 } else if (event.getAction() == Action.LEFT_CLICK_AIR) {
-                    execute(player, mainItemStack, Triggers.SNEAK_LEFT_CLICK);
-                    execute(player, offItemStack, Triggers.SNEAK_LEFT_CLICK);
+                    execute(player, Triggers.SNEAK_LEFT_CLICK);
                 }
             } else {
                 if (event.getAction() == Action.RIGHT_CLICK_AIR) {
-                    execute(player, mainItemStack, Triggers.RIGHT_CLICK);
-                    execute(player, offItemStack, Triggers.RIGHT_CLICK);
+                    execute(player, Triggers.RIGHT_CLICK);
                 } else if (event.getAction() == Action.LEFT_CLICK_AIR) {
-                    execute(player, mainItemStack, Triggers.LEFT_CLICK);
-                    execute(player, offItemStack, Triggers.LEFT_CLICK);
+                    execute(player, Triggers.LEFT_CLICK);
                 }
             }
         }
 
     }
 
-    public static void execute(HumanEntity humanEntity, ItemStack item, Triggers trigger) {
+    public static void execute(HumanEntity humanEntity, Triggers trigger) {
 
-        if (item == null || item.getType() == Material.AIR || !item.hasItemMeta()) {
-            HILogger.debugToConsole("Item is null or air or has no item meta.");
-            return;
-        }
+        HashMap<Integer, ItemStack> items = new HashMap<>();
+        items.put(0, humanEntity.getInventory().getItem(0)); // hb 0
+        items.put(1, humanEntity.getInventory().getItem(1)); // hb 1
+        items.put(2, humanEntity.getInventory().getItem(2)); // hb 2
+        items.put(3, humanEntity.getInventory().getItem(3)); // hb 3
+        items.put(4, humanEntity.getInventory().getItem(4)); // hb 4
+        items.put(5, humanEntity.getInventory().getItem(5)); // hb 5
+        items.put(6, humanEntity.getInventory().getItem(6)); // hb 6
+        items.put(7, humanEntity.getInventory().getItem(7)); // hb 7
+        items.put(8, humanEntity.getInventory().getItem(8)); // hb 8
+        items.put(36, humanEntity.getInventory().getItem(36)); // boots
+        items.put(37, humanEntity.getInventory().getItem(37)); // leggings
+        items.put(38, humanEntity.getInventory().getItem(38)); // chestplate
+        items.put(39, humanEntity.getInventory().getItem(39)); // helmet
+        items.put(40, humanEntity.getInventory().getItem(40)); // offhand
 
-        HistoriaItemData historiaItemData = HistoriaItemData.fromStack(item);
+        for (Entry<Integer, ItemStack> item : items.entrySet()) {
+            if (item != null) {
+                HistoriaItemData historiaItemData = HistoriaItemData.fromStack(item.getValue());
 
-        boolean hasExecutor = historiaItemData.hasData(EXECUTOR_KEY);
+                boolean hasExecutor = historiaItemData.hasData(EXECUTOR_KEY);
 
-        if (!hasExecutor) {
-            HILogger.debugToConsole("Item has no executor.");
-            return;
-        }
+                HILogger.debugToConsole("Checking item in slot " + item.getKey() + " for trigger " + trigger);
 
-        else {
-            ExecutorData executorData = historiaItemData.getData(EXECUTOR_KEY, ExecutorData.DATA_TYPE);
+                if (!hasExecutor) {
+                    continue;
+                } else {
+                    ExecutorData executorData = historiaItemData.getData(EXECUTOR_KEY, ExecutorData.DATA_TYPE);
 
-            executorData.execute(humanEntity, item, trigger);
+                    executorData.execute(humanEntity, item.getKey(), item.getValue(), trigger);
+                }
+            }
+
         }
     }
-
 }
